71f00049cde2b16ee93436cd23a1ddb080ebe543
José Luis López Sánchez <selutario@gmail.com>
2018-12-11 21:35:04 +0100

    Some gestures added

---
+++ NPI_Esfera/Assets/ChangeScene.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+
+public class ChangeScene : MonoBehaviour {
+
+    Controller m_leapController;
+    //Set these Textures in the Inspector
+    public Texture m_MainTexture, m_Normal, m_Metal;
+    Renderer m_Renderer;
+
+    Random rnd;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Fetch the Renderer from the GameObject
+        m_Renderer = GetComponent<Renderer>();
+
+        m_leapController = new Controller();
+
+        //Make sure to enable the Keywords
+        m_Renderer.material.EnableKeyword("_NORMALMAP");
+        m_Renderer.material.EnableKeyword("_METALLICGLOSSMAP");
+
+        //Set the Texture you assign in the Inspector as the main texture (Or Albedo)
+        m_Renderer.material.SetTexture("_MainTex", m_MainTexture);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Frame f = m_leapController.Frame();
+
+        // Obtenemos la mano izquierda y derecha
+        Hand left_hand = null;
+
+        for (int i = 0; i < f.Hands.Count; ++i)
+            if (f.Hands[i].IsLeft)
+                left_hand = f.Hands[i];
+
+
+        if (left_hand != null)
+        {
+            if (left_hand.GrabStrength > 0.9)
+            {
+
+                if (Random.Range(0, 2) == 0)
+                    m_Renderer.material.mainTexture = m_MainTexture;
+                else
+                    m_Renderer.material.mainTexture = m_Metal;
+
+            }
+
+        }   
+    }
+}
--- NPI_Esfera/Assets/MouseCamera.cs
+++ NPI_Esfera/Assets/MouseCamera.cs
@@ -8,46 +8,21 @@ public class MouseCamera : MonoBehaviour
     Controller m_leapController;
     bool m_twoHandGrabLastFrame = false;
     public Vector3 m_offset;
+    Bone index_finger;
 
-    public float speed = 1.5f;
+    public float speed = 0.1f;
     private float X;
     private float Y;
 
+    //Set these Textures in the Inspector
+    public Texture m_MainTexture, m_Normal, m_Metal;
+    Renderer m_Renderer;
+
     private void Start()
     {
+        // Leap init
         m_leapController = new Controller();
     }
-
-    //Hand GetLeftMostHand(Frame f)
-    //{
-    //    float xComp = float.MaxValue;
-    //    Hand candidate = null;
-    //    for (int i = 0; i < f.Hands.Count; ++i)
-    //    {
-    //        //if (f.Hands[i].PalmPosition.ToUnityScaled().x < xComp)
-
-    //        {
-    //            candidate = f.Hands[i];
-    //            //xComp = f.Hands[i].PalmPosition.ToUnityScaled().x;
-    //        }
-    //    }
-    //    return candidate;
-    //}
-
-    //Hand GetRightMostHand(Frame f)
-    //{
-    //    float xComp = -float.MaxValue;
-    //    Hand candidate = null;
-    //    for (int i = 0; i < f.Hands.Count; ++i)
-    //    {
-    //        if (f.Hands[i].PalmPosition.ToUnityScaled().x > xComp)
-    //        {
-    //            candidate = f.Hands[i];
-    //            xComp = f.Hands[i].PalmPosition.ToUnityScaled().x;
-    //        }
-    //    }
-    //    return candidate;
-    //}
 
     bool Pinching(Hand h)
     {
@@ -66,8 +41,6 @@ public class MouseCamera : MonoBehaviour
         Frame f = m_leapController.Frame();
 
         // Obtenemos la mano izquierda y derecha
-        //Hand left_hand = GetLeftMostHand(f);
-        //Hand right_hand = GetRightMostHand(f);
         Hand left_hand = null;
         Hand right_hand = null;
 
@@ -83,38 +56,47 @@ public class MouseCamera : MonoBehaviour
             }
         }
 
-
-
-        if (f.Hands.Count > 0)
+        if (right_hand != null)
         {
+            index_finger = right_hand.Fingers[1].Bone(Bone.BoneType.TYPE_DISTAL);
+
+
             float pitch = right_hand.Direction.Pitch;
-            float yaw = right_hand.Direction.Yaw;
+            float yaw = right_hand.Direction.Yaw + 0.5F;
+
+            //float pitch = index_finger.Direction.Pitch;
+            //float yaw = index_finger.Direction.Yaw;
+
+            Debug.Log("Pitch: " + pitch);
+            Debug.Log("Yaw: " + yaw);
+
+            if (Mathf.Abs(yaw) < 0.25F)
+            {
+                yaw = 0.0F;
+            }
+            if (Mathf.Abs(pitch) < 0.25F){
+                pitch = 0.0F;
+            }
 
             if ( right_hand.GrabStrength < 0.4)
             {
-            transform.Rotate(new Vector3(- pitch * speed, yaw * speed, 0));
+                transform.Rotate(new Vector3(- pitch * speed, yaw * speed, 0));
                 X = transform.rotation.eulerAngles.x;
                 Y = transform.rotation.eulerAngles.y;
                 transform.rotation = Quaternion.Euler(X, Y, 0);
-            }
-            else
-            {
-                transform.Rotate(new Vector3(0, 0, 0));
-                X = transform.rotation.eulerAngles.x;
-                Y = transform.rotation.eulerAngles.y;
-                transform.rotation = Quaternion.Euler(X, Y, 0);
+                //ChangeScene prueba;
+
+                //prueba.cambia();
             }
 
+            //else
+            //{
+            //    transform.Rotate(new Vector3(0, 0, 0));
+            //    X = transform.rotation.eulerAngles.x;
+            //    Y = transform.rotation.eulerAngles.y;
+            //    transform.rotation = Quaternion.Euler(X, Y, 0);
+            //}
         }
-
-
-        //if (Input.GetMouseButton(0))
-        //{
-        //    transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
-        //    X = transform.rotation.eulerAngles.x;
-        //    Y = transform.rotation.eulerAngles.y;
-        //    transform.rotation = Quaternion.Euler(X, Y, 0);
-        //}
     }
 }
 
