9e72c0d0252598b7eac7932cb4532418dd30588e
José Luis López Sánchez <selutario@gmail.com>
2018-12-12 20:45:26 +0100

    Zoom added with two fists

--- NPI_Esfera/Assets/MouseCamera.cs
+++ NPI_Esfera/Assets/MouseCamera.cs
@@ -10,30 +10,26 @@ public class MouseCamera : MonoBehaviour
     public Vector3 m_offset;
     Bone index_finger;
 
-    public float speed = 0.1f;
+    public float speed = 0f;
     private float X;
     private float Y;
-
-    //Set these Textures in the Inspector
-    public Texture m_MainTexture, m_Normal, m_Metal;
-    Renderer m_Renderer;
+    private float Z;
+    private float dist_anterior = 0;
+    private float camara_ini;
 
     private void Start()
     {
         // Leap init
         m_leapController = new Controller();
+
+        camara_ini = GetComponent<Camera>().fieldOfView;
+        Debug.Log("CAMARA_INI: " + camara_ini);
     }
 
     bool Pinching(Hand h)
     {
         if (h == null) return false;
         return h.PinchStrength > 0.7f;
-    }
-
-    bool Grabbing(Hand h)
-    {
-        if (h == null) return false;
-        return h.GrabStrength > 0.45f;
     }
 
     void Update()
@@ -44,58 +40,72 @@ public class MouseCamera : MonoBehaviour
         Hand left_hand = null;
         Hand right_hand = null;
 
+        float x_d = 0, y_d = 0, z_d = 0, 
+              x_i = 0, y_i = 0, z_i = 0;
+
+        bool closed_left = false, left = false,
+             closed_right = false, right = false; 
+
         for (int i = 0; i < f.Hands.Count; ++i)
         {
             if (f.Hands[i].IsLeft)
-            {
                 left_hand = f.Hands[i];
-            }
             else if (f.Hands[i].IsRight)
-            {
                 right_hand = f.Hands[i];
-            }
         }
 
-        if (right_hand != null)
+        if (right = (right_hand != null))
         {
-            index_finger = right_hand.Fingers[1].Bone(Bone.BoneType.TYPE_DISTAL);
+            float pitch = right_hand.Direction.Pitch,
+                  yaw = right_hand.Direction.Yaw + 0.5F;
 
-
-            float pitch = right_hand.Direction.Pitch;
-            float yaw = right_hand.Direction.Yaw + 0.5F;
-
-            //float pitch = index_finger.Direction.Pitch;
-            //float yaw = index_finger.Direction.Yaw;
-
-            Debug.Log("Pitch: " + pitch);
-            Debug.Log("Yaw: " + yaw);
+            x_d = right_hand.PalmPosition.x;
+            y_d = right_hand.PalmPosition.y;
+            z_d = right_hand.PalmPosition.z;
+            //Debug.Log("POS_DER: (" + x_d + ", " + y_d + ", " + z_d + ")");
 
             if (Mathf.Abs(yaw) < 0.25F)
-            {
                 yaw = 0.0F;
-            }
-            if (Mathf.Abs(pitch) < 0.25F){
+            if (Mathf.Abs(pitch) < 0.25F)
                 pitch = 0.0F;
-            }
 
-            if ( right_hand.GrabStrength < 0.4)
+            Debug.Log("GRAB: " + right_hand.GrabStrength);
+
+            if (right_hand.GrabStrength < 1)
             {
-                transform.Rotate(new Vector3(- pitch * speed, yaw * speed, 0));
+                transform.Rotate(new Vector3(-pitch * speed, yaw * speed, 0));
                 X = transform.rotation.eulerAngles.x;
                 Y = transform.rotation.eulerAngles.y;
                 transform.rotation = Quaternion.Euler(X, Y, 0);
-                //ChangeScene prueba;
-
-                //prueba.cambia();
             }
+            else
+                closed_right = true;
+        }
+        if (left = (left_hand != null))
+        {
+            x_i = left_hand.PalmPosition.x;
+            y_i = left_hand.PalmPosition.y;
+            z_i = left_hand.PalmPosition.z;
+            //Debug.Log("POS_IZQ: (" + x + ", " + y + ", " + z + ")");
 
-            //else
-            //{
-            //    transform.Rotate(new Vector3(0, 0, 0));
-            //    X = transform.rotation.eulerAngles.x;
-            //    Y = transform.rotation.eulerAngles.y;
-            //    transform.rotation = Quaternion.Euler(X, Y, 0);
-            //}
+            if (left_hand.GrabStrength >= 1)
+                closed_left = true;
+        }
+        if (left && right) {
+            float x_2 = (x_d - x_i) * (x_d - x_i),
+                  y_2 = (y_d - y_i) * (y_d - y_i),
+                  z_2 = (z_d - z_i) * (z_d - z_i);
+
+            float distancia = Mathf.Sqrt(x_2 + y_2 + z_2);
+            float diff_distancia = distancia - dist_anterior;
+            dist_anterior = distancia;
+            //Debug.Log("DIST: " + diff_distancia);
+            Debug.Log("CAMARA_INI: " + camara_ini);
+            if (closed_left && closed_right)
+            {
+                if (diff_distancia != 0)
+                    GetComponent<Camera>().fieldOfView -= diff_distancia / 10;
+            }
         }
     }
 }
